bc5b35d28f16206684f60b8ac2430961fb91fe54
frmdstryr <frmdstryr@protonmail.com>
2020-04-20 19:15:02 -0400

    Update tests and re-add azure since github workflows don't work on windows

--- test/test_modules.py
+++ test/test_modules.py
@@ -16,16 +16,65 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+import os
 import sys
-import json
 import pytest
-from os.path import join, dirname
+import subprocess
+import OCCT
 
-# Collect all the modules
-BASE_DIR = dirname(dirname(__file__))
-with open(join(BASE_DIR, 'binder', 'generate', 'all_modules.json')) as f:
-    ALL_MODULES = json.load(f)
 
+PY_EXT = '.pyd' if sys.platform == 'win32' else '.so'
+OCCT_PATH = OCCT.__path__[0]
+
+def get_modules():
+    files = sorted(os.listdir(OCCT_PATH))
+    modules = [p.split(".")[0] for p in files if p.endswith(PY_EXT)]
+
+    if sys.platform == 'win32':
+        excludes = ('Cocoa', 'Xw', 'XwWindow')
+    elif sys.platform == 'darwin':
+        excludes = ('WNT', 'Xw', 'XwWindow')
+    else:
+        excludes = ('WNT', 'Cocoa')
+
+    return [m for m in modules if m not in excludes]
+
+def debug_module(mod):
+    mod_path = os.path.join(OCCT_PATH, f'{mod}{PY_EXT}')
+    if sys.platform == 'win32':
+        run_cmd(f'dumpbin /dependents {mod_path}')
+    else:
+        run_cmd(f'ldd {mod_path}')
+        out1 = run_cmd(f'nm -gD {mod_path}', verbose=False)
+        out2 = run_cmd(f'nm -gDC {mod_path}', verbose=False)
+        for a, b in zip(out1.splitlines(), out2.splitlines()):
+            print(a)
+            # Print demangled output
+            if a != b:
+                print(b)
+                print("")
+
+def run_cmd(cmd, grep=None, exclude=None, verbose=True):
+    if verbose:
+        print(cmd)
+    output = subprocess.check_output(cmd.split()).decode()
+    if verbose:
+        for line in output.splitlines():
+            if not grep or grep in line:
+                if exclude and exclude in line:
+                    continue
+                print(line)
+    return output
+
+
+ALL_MODULES = get_modules()
 @pytest.mark.parametrize('mod', ALL_MODULES)
 def test_module(mod):
-    __import__(f'OCCT.{mod}')
+    try:
+        __import__(f'OCCT.{mod}')
+    except ImportError as e:
+        print(e)
+        debug_module(mod)
+        if 'IVtk' in mod:
+            pytest.xfail("IVtk errors are ignored")
+        raise
