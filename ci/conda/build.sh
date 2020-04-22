073433cd9020c5c92bd67e17bc7ef3a425a6fe58
frmdstryr <frmdstryr@protonmail.com>
2020-04-22 09:47:47 -0400

    Re-add -fpermissive

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -10,6 +10,11 @@ rm -Rf build
 mkdir build
 cd build
 
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export CPPFLAGS="${CPPFLAGS} -framework OpenGL"
+  export LDFLAGS="${LDFLAGS} -framework OpenGL"
+fi
+
 cmake .. -G "Ninja" \
     -DCMAKE_BUILD_TYPE="Release" \
     -DENABLE_SMESH=OFF \
