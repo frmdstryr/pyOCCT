6ef77809cc7c642f9c5cb04cef92d18fdda4e7a1
frmdstryr <frmdstryr@protonmail.com>
2025-05-26 19:51:38 -0400

    Fix build and imports

--- test/test_imports.py
+++ test/test_imports.py
@@ -1,16 +1,17 @@
 import unittest
+import OCCT
 from os.path import abspath, dirname, split, splitext
 from glob import glob
 
 class Test_ModulesImport(unittest.TestCase):
     def testModulesImport(self):
         errors = []
-        root = abspath(dirname(dirname(__file__)))
+        root = OCCT.__path__[0]
 
         mods = []
-        for f in glob(f'{root}/src/*.cxx'):
-            mod, _ = splitext(split(f)[1])
-            if mod.endswith("_2"):
+        for f in glob(f'{root}/*'):
+            mod, ext = splitext(split(f)[1])
+            if mod.startswith("_") and ext:
                 continue
             mods.append(f'OCCT.{mod}')
 
